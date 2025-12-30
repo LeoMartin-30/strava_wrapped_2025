b1d4f1ff5c6bc5903516abc45a527fba1e6804eb
Leo Martin <leomoto30@gmail.com>
2025-12-30 22:32:16 +0100

    vercel/analytics/react to vercel/analytics/next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
